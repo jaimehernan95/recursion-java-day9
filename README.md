fb8dd581823429dc4de667ebd712cfeb9c010765
jaimea <jaime.hernan95@gmail.com>
2020-03-27 02:33:45 -0400

    factorial new image

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Result
 
 <img width="964" alt="recursion-java-day9" src="https://github.com/jaimehernan95/recursion-java-day9/blob/master/image/recursion.png">
 
+# Multiply Factorial
+
+
+Result
+
+<img width="964" alt="factorial" src="https://github.com/jaimehernan95/recursion-java-day9/blob/master/image/MultiplyFactorial.png">
+
--- src/FactorialExcercise.java
+++ src/FactorialExcercise.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class FactorialExcercise {
-}
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            System.out.println(" Enter a number to calculate the factorial : ");
+            int n = in.nextInt();
+            in.close();
+
+            System.out.println("The factorial of " + n + " is = " + factorial(n));
+        }
+
+        private static int factorial(int n) {
+            if (n == 1) return 1;
+            return factorial(n - 1) * n;
+        }
+    }
